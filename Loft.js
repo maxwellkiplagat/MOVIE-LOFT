62683e02e98f384f8d2976625fb822ad39c1dbef
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-03-30 11:12:13 +0300

    added the DOM element and put them in a varibale

---
+++ Loft.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',  function(){
+//adding my DOM element and put the in variable
+    const searchInput = document.getElementById('search-input');
+    const searchButton = document.getElementById('search-button');
+    const showAllButton = document.getElementById('show-all-button');
+    const genreDropdown = document.getElementById('genre-dropdown');
+    const moviesContainer = document.getElementById('movies-container');
+    const movieTemplate = document.getElementById('movie-template');
+    const myform = document.getElementById('dataForm')
+
+})
