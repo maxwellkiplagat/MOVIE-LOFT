1ec12143cce2605e1206e2ffd8eaccbdcb4b93d0
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-03-30 11:19:03 +0300

    added a finction fetch data from the URL and displayin data in the database

--- Loft.js
+++ Loft.js
@@ -7,5 +7,25 @@ document.addEventListener('DOMContentLoaded',  function(){
     const moviesContainer = document.getElementById('movies-container');
     const movieTemplate = document.getElementById('movie-template');
     const myform = document.getElementById('dataForm')
+    //varible to keep movie array after fetching
+    let allMovies = [];
+    //function to fetch the data from the url and siplaying the first movie in the db
+    function loadFirstMovies() {
+        fetch('http://localhost:3000/movies')
+            .then(response => response.json())
+            .then(data => {
+                allMovies = data;
+                
+                if (allMovies.length > 0) {
+                    displayMovie(allMovies[0]);
+                } else {
+                    alert('No movies available');
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
 
 })
